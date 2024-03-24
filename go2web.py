3512ce38f5aa7ac09124e6165bd245ce3da84af9
DanHariton <danhariton22@gmail.com>
2024-03-24 12:51:40 +0200

    small fix

--- go2web.py
+++ go2web.py
@@ -1,18 +1,20 @@
 import sys
 import socket
-import urllib.parse
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 
 def make_http_request(url):
     try:
-        parsed_url = urllib.urlparse(url)
+        parsed_url = urlparse(url)
         with socket.create_connection((parsed_url.netloc, 80)) as s:
-            s.sendall(f"GET {parsed_url.path or '/'} HTTP/1.1\r\nHost: {parsed_url.netloc}\r\nConnection: close\r\n\r\n".encode())
+            s.sendall(
+                f"GET {parsed_url.path or '/'} HTTP/1.1\r\nHost: {parsed_url.netloc}\r\nConnection: close\r\n\r\n".encode())
             response = b''.join(iter(lambda: s.recv(1024), b''))
         return BeautifulSoup(response, 'html.parser').get_text()
     except Exception as e:
         return f"Error: {e}"
+
 
 def search(search_term):
     try:
@@ -28,10 +30,13 @@ def search(search_term):
     except Exception as e:
         return f"Error: {e}"
 
+
 def print_help():
     print("go2web -u <URL>         # make an HTTP request to the specified URL and print the response")
     print("go2web -s <search-term> # make an HTTP request to search the term using Google and print top 10 results")
     print("go2web -h               # show this help")
+
+
 def main():
     args = sys.argv[1:]
     if len(args) < 2 or args[0] not in ['-u', '-s', '-h']:
@@ -40,7 +45,7 @@ def main():
 
     option, arg = args[:2]
     actions = {'-u': lambda: make_http_request(arg),
-               # '-s': lambda: search(arg),
+               '-s': lambda: search(arg),
                '-h': print_help}
 
     print(actions.get(option, lambda: "Invalid option. Use '-h' for help.")())
@@ -48,4 +53,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
